7edc9cd10d8c4532e18311849be3f6d66129078b
Khaki01 <mkhaknazar@kaist.ac.kr>
2023-05-03 21:41:55 +0900

    battery basic

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import dash
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 
-app = dash.Dash(__name__)
+from calendarUtils import hello
 
-app.layout = html.Div(
-    [
-        html.Div(
+# from dash_extensions import DeferScript
+
+# external CSS stylesheets
+external_stylesheets = [
+    # 'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    # {
+    #     'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+    #     'rel': 'stylesheet',
+    #     'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+    #     'crossorigin': 'anonymous'
+    # }
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    dbc.themes.BOOTSTRAP,
+]
+
+app = dash.Dash(
+    external_stylesheets=external_stylesheets
+)
+
+battery_layout = html.Div(
             [
                 html.Div(
                     [],
@@ -27,7 +45,214 @@ app.layout = html.Div(
                 ),
             ],
             className="battery",
-        ),
+        )
+
+calendar_container = dbc.Container(
+            [
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.H2(
+                                    "Calendar #004",
+                                    className="heading-section",
+                                ),
+                            ],
+                            className="col-md-6 text-center mb-5",
+                        ),
+                    ],
+                    className="row justify-content-center",
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.Div(
+                                    [],
+                                    className="content w-100",
+                                ),
+                                html.Div(
+                                    [
+                                        html.Div(
+                                            [
+                                                html.Span(
+                                                    className="left-button fa fa-chevron-left",
+                                                    id="prev",
+                                                ),
+                                                html.Span(
+                                                    className="year",
+                                                    id="label",
+                                                ),
+                                                html.Span(
+                                                    className="right-button fa fa-chevron-right",
+                                                    id="next"
+                                                )
+                                            ],
+                                            className="year-header",
+                                        ),
+                                        dbc.Table(
+                                            [
+                                                html.Tbody(
+                                                    [
+                                                        html.Tr(
+                                                            [
+                                                                html.Td(
+                                                                    "Jan",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Feb",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Mar",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Apr",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "May",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Jun",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Jul",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Aug",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Sep",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Oct",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Nov",
+                                                                    className="month"
+                                                                ),
+                                                                html.Td(
+                                                                    "Dec",
+                                                                    className="month"
+                                                                )
+                                                            ],
+                                                            className="months-row"
+                                                        ),
+                                                    ],
+                                                )
+                                            ],
+                                            class_name="months-table w-100"
+                                        ),
+                                        dbc.Table(
+                                            [
+                                                html.Tbody(
+                                                    [
+                                                        html.Tr(
+                                                            [
+                                                                html.Td(
+                                                                    "Sun",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Mon",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Tud",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Wed",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Thu",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Fri",
+                                                                    className="day"
+                                                                ),
+                                                                html.Td(
+                                                                    "Sat",
+                                                                    className="day"
+                                                                )
+                                                            ],
+                                                        )
+                                                    ],
+                                                )
+                                            ],
+                                            class_name="days-table w-100"
+                                        ),
+                                        html.Div(
+                                            [
+                                                dbc.Table(
+                                                    [
+                                                        html.Tbody(
+                                                            className="tbody",
+                                                        )
+                                                    ],
+                                                    class_name="dates-table w-100",
+                                                ),
+                                            ],
+                                            className="frame",
+                                        ),
+                                        html.Button(
+                                            "Add Event",
+                                            className="button",
+                                            id="add-button",
+                                        ),
+                                    ],
+                                    className="calendar-container",
+                                ),
+                                html.Div(
+                                    [],
+                                    className="events-container",
+                                ),
+                                html.Div(
+                                    [
+                                        html.H2(
+                                            "Add New Event",
+                                            className="dialog-header",
+                                        ),
+                                        html.Form(
+                                            [
+                                                html.Div(
+                                                    [],
+                                                    className="form-container",
+                                                ),
+                                            ],
+                                            className="form",
+                                            id="form",
+                                        ),
+                                    ],
+                                    className="dialog",
+                                    id="dialog",
+                                ),
+                            ],
+                            className="col-md-12",
+                        ),
+                    ],
+                    className="row",
+                )
+            ],
+            class_name="container"
+
+        )
+
+app.layout = html.Div(
+    [
+        battery_layout, 
+        calendar_container,
     ],
     className="main",
 )
@@ -40,7 +265,8 @@ app.layout = html.Div(
 )
 def callback(n_clicks, style):
     print(style)
-    return {'width': '40%'}
+    hello()
+    # return {'width': '40%'}
 
 
 if __name__ == '__main__':
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,165 @@
-alert("If you see this alert, then your custom JavaScript script has run!");
+window.addEventListener("load", function () {
+  var date = new Date();
+  var today = date.getDate();
+  //   click handler
+  var rightBtn = document.getElementsByClassName("right-button")[0];
+  var leftBtn = document.getElementsByClassName("left-button")[0];
+  var monthBox = document.getElementsByClassName("month");
+  var addBtn = this.document.getElementById("add-button");
+  rightBtn.addEventListener("click", () => {
+    next_year(date);
+  });
+  leftBtn.addEventListener("click", () => {
+    prev_year(date);
+  });
+  for (var i = 0; i < monthBox.length; i++) {
+    monthBox[i].addEventListener("click", (e) => {
+      month_click(e, date, i);
+    });
+  }
+  addBtn.addEventListener("click", toggleFunction);
 
-let button = document.getElementById("battery-body");
-
-button.addEventListener("click", function () {
-  button.style.height = "50%";
+  function toggleFunction() {
+    console.log(today);
+  }
+  init_calendar(date);
 });
+
+function init_calendar(date) {
+  var tbodyBox = document.getElementsByClassName("tbody")[0];
+  while (tbodyBox.lastChild) {
+    tbodyBox.removeChild(tbodyBox.lastChild);
+  }
+  //   $(".tbody").empty();
+  var eventsContainer = document.getElementsByClassName("events-container")[0];
+  while (eventsContainer.lastChild) {
+    eventsContainer.removeChild(eventsContainer.lastChild);
+  }
+  //   $(".events-container").empty();
+  var calendar_days = tbodyBox;
+  var month = date.getMonth();
+  var year = date.getFullYear();
+  var day_count = days_in_month(month, year);
+  var row = document.createElement("tr");
+  row.classList.add("table-row");
+  //   var row = $("<tr class='table-row'></tr>");
+  var today = date.getDate();
+  // Set date to 1 to find the first day of the month
+  date.setDate(1);
+  var first_day = date.getDay();
+  // 35+firstDay is the number of date elements to be added to the dates table
+  // 35 is from (7 days in a week) * (up to 5 rows of dates in a month)
+  for (var i = 0; i < 35 + first_day; i++) {
+    // Since some of the elements will be blank,
+    // need to calculate actual date from index
+    var day = i - first_day + 1;
+    // If it is a sunday, make a new row
+    if (i % 7 === 0) {
+      calendar_days.append(row);
+      row = document.createElement("tr");
+      row.classList.add("table-row");
+      //   row = $("<tr class='table-row'></tr>");
+    }
+    // if current index isn't a day in this month, make it blank
+    if (i < first_day || day > day_count) {
+      var curr_date = document.createElement("td");
+      curr_date.classList.add("table-date", "nil");
+      //   var curr_date = $("<td class='table-date nil'>" + "</td>");
+      row.append(curr_date);
+    } else {
+      var curr_date = document.createElement("td");
+      curr_date.classList.add("table-date", "nil");
+      curr_date.innerHTML = day;
+      //   var curr_date = $("<td class='table-date'>" + day + "</td>");
+      //   var events = check_events(day, month + 1, year);
+      var activeDate = document.getElementsByClassName("active-date");
+      if (today === day && !activeDate) {
+        curr_date.addClass("active-date");
+        // show_events(events, months[month], day);
+      }
+      // If this date has any events, style it with .event-date
+      //   if (events.length !== 0) {
+      //     curr_date.addClass("event-date");
+      //   }
+      // Set onClick handler for clicking a date
+      //   curr_date.click(
+      //     { events: events, month: months[month], day: day },
+      //     date_click
+      //   );
+      row.append(curr_date);
+    }
+  }
+  // Append the last row and set the current year
+  calendar_days.append(row);
+  var yearBox = document.getElementsByClassName("year")[0];
+  yearBox.innerHTML = year;
+  //   $(".year").text(year);
+}
+
+function days_in_month(month, year) {
+  var monthStart = new Date(year, month, 1);
+  var monthEnd = new Date(year, month + 1, 1);
+  return (monthEnd - monthStart) / (1000 * 60 * 60 * 24);
+}
+
+// Event handler for when a month is clicked
+function month_click(event, date, index) {
+  console.log("asdf");
+  var eventsContainer = document.getElementsByClassName("events-container")[0];
+  eventsContainer.style.display = "block";
+  //   $(".events-container").show(250);
+  var dialogBox = document.getElementById("dialog");
+  dialogBox.style.display = "none";
+  var date = date;
+  var activeMonth = document.getElementsByClassName("active-month")[0];
+  if (activeMonth) activeMonth.classList.remove("active-month");
+  event.target.classList.add("active-month");
+  //   $(this).addClass("active-month");
+  //   var new_month = $(".month").index(this);
+  date.setMonth(index);
+  console.log(index);
+  //   init_calendar(date);
+}
+
+// Event handler for when the year right-button is clicked
+function next_year(date) {
+  var dialogBox = document.getElementById("dialog");
+  dialogBox.style.display = "none";
+  var date = date;
+  var new_year = date.getFullYear() + 1;
+  var yearBox = document.getElementsByClassName("year")[0];
+  yearBox.innerHTML = new_year;
+  date.setFullYear(new_year);
+  //   init_calendar(date);
+}
+
+// Event handler for when the year left-button is clicked
+function prev_year(date) {
+  var dialogBox = document.getElementById("dialog");
+  dialogBox.style.display = "none";
+  //   setTimeout(() => {
+  //     // 👇️ removes element from DOM
+  //     dialogBox.style.display = "block";
+  //   }, 250);
+  var date = date;
+  var new_year = date.getFullYear() - 1;
+  var yearBox = document.getElementsByClassName("year")[0];
+  yearBox.innerHTML = new_year;
+  date.setFullYear(new_year);
+  //   init_calendar(date);
+}
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
